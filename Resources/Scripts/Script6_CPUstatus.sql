bb2bfaa682ec6131218409d8324e819d0a70d16d
judsonhayes <judsonhayes@users.noreply.github.com>
2018-10-01 20:40:53 -0500

    comments only, no functional change

--- Resources/Scripts/Script6_CPUstatus.sql
+++ Resources/Scripts/Script6_CPUstatus.sql
@@ -1,4 +1,4 @@
-select top 50 
+select top 50 --returns the cpu utilization, how long the cpu is working, and the number of instructions processed 
     sum(qs.total_worker_time) as total_cpu_time, 
     sum(qs.execution_count) as total_execution_count,
     count(*) as  number_of_statements, 
